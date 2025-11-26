a3d358fefc663ce2e7fbba4a11c8d52ee2246eac
tkb0005053 <tkb5053ivan@gmail.com>
2025-11-26 11:20:12 +0800

    feat: 訂單專案實作 OpenFeign 呼叫庫存服務

--- order-service/src/main/java/com/techie/mocroservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/techie/mocroservices/order/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.techie.mocroservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OrderServiceApplication {
 
--- order-service/src/main/java/com/techie/mocroservices/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/techie/mocroservices/order/service/impl/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package com.techie.mocroservices.order.service.impl;
 
+import com.techie.mocroservices.order.client.InventoryClient;
 import com.techie.mocroservices.order.model.Order;
 import com.techie.mocroservices.order.model.OrderRequest;
 import com.techie.mocroservices.order.repo.OrderRepository;
 import com.techie.mocroservices.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -14,16 +17,25 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     @Override
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setId(orderRequest.id());
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isInStock){
+            Order order = new Order();
+            order.setId(orderRequest.id());
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else {
+            throw new ResponseStatusException(HttpStatus.CONFLICT,
+                    "Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
     }
 
 }
